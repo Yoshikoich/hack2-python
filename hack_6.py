5e37e5aa54f5a022c9aa753b322ad0211a63300e
Andrea <andreamigoatencio@gmail.com>
2024-07-29 12:43:48 -0400

    feat: resuelto 6

--- hack_6.py
+++ hack_6.py
@@ -9,7 +9,8 @@ text: [] output => ["0"]
 """
 
 
-def fn_hack_6(s):
-    result = s
-    #...
+def fn_hack_6(lst):
+    if not lst:
+        return ["0"]
+    result = [str(i+1) if i % 2 == 0 else "-" for i in range(len(lst))]
     return result
