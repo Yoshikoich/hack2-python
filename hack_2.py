1a52be4c8db3a697a8ee0542f80462a9e2946ac0
Andrea <andreamigoatencio@gmail.com>
2024-07-29 11:51:08 -0400

    feat: 2 resuelto

--- hack_2.py
+++ hack_2.py
@@ -8,6 +8,13 @@ text: "qux" output => "qx"
 
 
 def fn_hack_2(s):
-    result = s
-    #...
+
+    vowels = ["a", "e", "i", "o", "u"]
+    _str = []
+    
+    for char in s:
+        if char not in vowels:
+            _str.append(char)
+    
+    result = "".join(_str)
     return result
